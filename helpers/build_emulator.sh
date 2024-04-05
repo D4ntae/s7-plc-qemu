854c9dc1753da3fe95bd8a680c83bee2514f22b7
D4ntae <danko.delimar@gmail.com>
2024-04-05 08:21:34 +0200

    Update build scripts

--- helpers/build_emulator.sh
+++ helpers/build_emulator.sh
@@ -3,7 +3,8 @@
 file="$1"
 dir=$(dirname "$0")
 ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." &>/dev/null && pwd)"
-
+echo "[i] Cleaning old build..."
+rm -rf $ROOT_DIR/build
 echo "[i] Starting build"
 mkdir $ROOT_DIR/build
 cd $ROOT_DIR/build
